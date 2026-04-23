98a5a84cb4f58ea296a6a1407c7c2f79f50bfa38
Chandan Puggal <85604768+chandanpuggal@users.noreply.github.com>
2026-04-23 21:11:21 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+
+    // Sorting
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return !s.compare(t);
+    }
+};
