4a70d05fff83c2ba598c847a800ed77fba0b98ac
Chandan Puggal <85604768+chandanpuggal@users.noreply.github.com>
2026-07-03 11:34:43 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    // LeetCode: https://leetcode.com/problems/valid-anagram/description/?envType=problem-list-v2&envId=5zd3o9vd
+    // Video: https://www.youtube.com/watch?v=9UtInBqnCgA
+
+    // Sorting
+    /*
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return !s.compare(t);
+    }
+    */
+
+    // hashMap and Count
+    bool isAnagram(string s, string t) {
+
+        if (s.length() != t.length())
+            return false;
+
+        map<char, int> sMap;
+        map<char, int> tMap;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (sMap.find(s[i]) == sMap.end()) {
+                sMap[s[i]] = 1;
+            } else {
+                sMap[s[i]]++;
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (tMap.find(t[i]) == tMap.end()) {
+                tMap[t[i]] = 1;
+            } else {
+                tMap[t[i]]++;
+            }
+        }
+
+        // compare function work internally like char or int comparaision.
+        // This checks that:
+            // Both maps have the same keys.
+            // Each key has the same corresponding value.
+        return (sMap == tMap);
+    }
+};
