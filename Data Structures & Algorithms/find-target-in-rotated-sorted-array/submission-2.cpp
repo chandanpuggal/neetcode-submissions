5d62adc6a5d5c5d23f34cb83547fa226113ec3cb
Chandan Puggal <85604768+chandanpuggal@users.noreply.github.com>
2026-07-08 19:13:38 +0530

    Add: find-target-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    // LeetCode: https://leetcode.com/problems/search-in-rotated-sorted-array/description/?envType=problem-list-v2&envId=5zd3o9vd
+    // Video: https://www.youtube.com/watch?v=U8XENwh8Oy8
+
+    int search(vector<int>& nums, int target) {
+        int index = -1;
+        int left = 0, right = nums.size() - 1, mid = 0;
+
+        while (left <= right) {
+            mid = left + (right - left)/2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            // left portion is sorted
+            if (nums[mid] >= nums[left])  {
+                if ((nums[mid] >= target) && (nums[left] <= target)) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target <= nums[mid]) {
+                    right = mid;
+                } else if (target > nums[right]) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }      
+        }
+        return -1;
+    }
+};
